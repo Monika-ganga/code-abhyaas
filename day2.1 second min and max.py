cdc4de3c4bb601a9d6013c66e21fb432573ed608
Monika-ganga <131649524+Monika-ganga@users.noreply.github.com>
2023-08-24 20:13:03 +0530

    Update day2.1 second min and max.py

--- day2.1 second min and max.py
+++ day2.1 second min and max.py
@@ -9,13 +9,14 @@ for i in range(len(a)):
         first_max=a[i]
     if(a[i]>second_max and a[i]!=first_max):
         second_max=a[i]
-print("second maximum is:"second_max)
+print("second maximum is:",second_max)
 for i in range(len(a)):
     if a[i]<first_min:
         second_min=first_min
-        first_max=a[i]
-    if(a[i]>second_min and a[i]!=first_min):
+        first_min=a[i]
+    if(a[i]<second_min and a[i]!=first_min):
         second_min=a[i]
-print("second minimum number is:"second_min)
+print("second minimum number is:",second_min)
 
+        
         
