d46d66e94cfe399844c637ec69462c6c1ad7394a
Monika-ganga <131649524+Monika-ganga@users.noreply.github.com>
2023-08-31 01:19:52 +0530

    Add files via upload

---
+++ arraysort0,1,2.py
@@ -0,0 +1,16 @@
+a=[0,1,1,0,1,2,1,2,0,0,0]
+low=0
+mid=0
+high=len(a)-1
+print(a)
+while(mid<=high):
+    if(a[mid]==0):
+        a[mid],a[low]=a[low],a[mid]
+        low+=1
+        mid+=1
+    elif(a[mid]==1):
+        mid+=1
+    else:
+        a[mid],a[high]=a[high],a[mid]
+        high-=1
+print(a)
