c165a8c60cefa8b4b7541fa73426095f8456c6eb
Monika-ganga <131649524+Monika-ganga@users.noreply.github.com>
2023-09-07 14:35:51 +0530

    Update majority n by 3.py

--- majority n by 3.py
+++ majority n by 3.py
@@ -1,22 +1,33 @@
-a=[7,7,5,7,5,1,5,7,5,5,7,7,5,5,5,5]
-element=a[0]
-n=len(a)
-count=1
-for i in range(1,len(a)):
-    if(a[i]==element):
-        count+=1
-    else:
-        count-=1
-        if(count==0):
-            element=a[i]
-            count=1
-print(count)
+a=[2,2,1,3,1,1,3,1,1]
 count1=0
+count2=0
+element1=None
+element2=None
+ans=set()
+n=len(a)
 for i in range(len(a)):
-    if(a[i]==element):
+    if(count1==0 and a[i]!=element2):
+        element1=a[i]
+        count1=1
+    if(count2==0 and a[i]!=element1):
+        element2=a[i]
+        count2=1
+    if(a[i]==element1):
         count1+=1
-print(count1)
-if(count1>n/3):
-    print(element)
-else:
-    print(-1)
+    if(a[i]==element2):
+        count2+=1
+    else:
+        count1-=1
+        count2-=1
+major1=0
+major2=0
+for i in range(len(a)):
+    if(a[i]==element1):
+        major1+=1
+    if(a[i]==element2):
+        major2+=1
+if(major1>n//3):
+    ans.add(element1)
+if(major2>n//3):
+    ans.add(element2)
+print(ans)
